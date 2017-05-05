f9c9baf152a91ce835cac87758777114d018ed15
pskyv <pskyvalidas@gmail.com>
2017-05-06 01:08:29 +0300

    feat: Make patients menu item click to return patients as json

---
+++ DentalAssist/DentalAssist/Controllers/PatientsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DentalAssist.Services;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DentalAssist.Controllers
+{
+    public class PatientsController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public PatientsController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        // GET: /<controller>/
+        public JsonResult Index()
+        {
+            return Json(_unitOfWork.PatientRepository.GetAll());
+        }
+    }
+}
